de9ed480903bbe0d4eb811102528df38b1af1cb9
Jonathan <jonathan.gualli@epn.edu.ec>
2022-12-03 16:36:07 -0500

    Se añadio un comentario a la rama master desde mi computadora

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -6,6 +6,7 @@ public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Nueva prueba Hola mundo");
         System.out.println("comentario - Commit desde computadora Santiago");
+        System.out.println("Comentario2 - commit desde computadora mia");
     }
     
 }
