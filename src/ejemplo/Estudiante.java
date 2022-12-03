8f5be138bff9f416e516b843f086131f84868b97
Jgualli <jonifabricio11@gmail.com>
2022-12-03 16:48:55 -0500

    se agrego titulo a la clase estudiante desde computadora santiago

--- src/ejemplo/Estudiante.java
+++ src/ejemplo/Estudiante.java
@@ -4,5 +4,6 @@ package ejemplo;
 public class Estudiante {
     public void imprimirEstudiante(){
         System.out.println("Soy un estudiante");
+        System.out.println("titulo - comit desde computadora sntiago");
     }
 }
