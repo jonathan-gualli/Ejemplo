5bc654e9b93aef71c67e02eab8a22dc090dab785
Jonathan <jonathan.gualli@epn.edu.ec>
2022-12-03 16:25:53 -0500

    Comentario desde mi computadora

--- src/ejemplo/Asignatura.java
+++ src/ejemplo/Asignatura.java
@@ -4,5 +4,6 @@ package ejemplo;
 public class Asignatura {
     public void imprimirAsignatura(){
         System.out.println("Soy una asigantura");
+        System.out.println("Comentario desde mi computadora");
     }
 }
