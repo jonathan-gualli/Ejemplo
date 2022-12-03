16b4a877171f521b5704918da069137f5ca4cc42
Jgualli <jonifabricio11@gmail.com>
2022-12-03 15:50:22 -0500

    se agrago comentario desde computadora santiago

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -5,6 +5,7 @@ public class Ejemplo {
 
     public static void main(String[] args) {
         System.out.println("Nueva prueba Hola mundo");
+        System.out.println("comentario - Commit desde computadora Santiago");
     }
     
 }
